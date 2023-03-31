f304c37a21e5c6b73b5675b2919e9a46f73518f5
Ergothkampfgaist94 <aflopez9420@gmail.com>
2023-03-31 18:29:46 -0500

    a1

--- DragonsWay.Logic/Game.cs
+++ DragonsWay.Logic/Game.cs
@@ -2,6 +2,15 @@
 {
     public class Game
     {
+        private char [,] _deathscavern;
 
+        public int N { get;}
+        public String Way { get;}
+        public Game(int n, string way)
+        {
+            N= n;
+            Way = way;
+            _deathscavern = new char[N,N*2];
+        }
     }
 }
